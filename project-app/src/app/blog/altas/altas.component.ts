b054b598b7ea38e7c1923f1b0a216ffd0b178a13
clara fernandez <clara.fernandez@babel.es>
2018-03-13 13:58:02 +0100

    altas y lista

---
+++ project-app/src/app/blog/altas/altas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-altas',
+  templateUrl: './altas.component.html',
+  styles: []
+})
+export class AltasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
