eaed954fe97dbcb12439e51d003f661b36df37cf
clara fernandez <clara.fernandez@babel.es>
2018-03-13 13:59:32 +0100

    changes

--- project-app/src/app/about/about.module.ts
+++ project-app/src/app/about/about.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    FormsModule
   ],
-  declarations: [AboutComponent]
+  declarations: [
+    AboutComponent,
+    FormComponent
+  ],
+  exports: [
+    AboutComponent
+  ]
 })
 export class AboutModule { }
