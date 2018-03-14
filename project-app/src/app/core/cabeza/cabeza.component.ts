c271650e8683354a3c7cf0d7f1ff52128761aaab
clara fernandez <clara.fernandez@babel.es>
2018-03-14 13:58:43 +0100

    cabeza y home

--- project-app/src/app/core/cabeza/cabeza.component.ts
+++ project-app/src/app/core/cabeza/cabeza.component.ts
@@ -15,23 +15,7 @@ export class CabezaComponent implements OnInit {
   }
 
   toggleMenu() {
-    console.log('toggle');
-    // this.estado === 'none' ? 'block' : 'none';
-  }
-
-  /*
-        const menuList = document.querySelector('#menu-list');
-        const navBars = document.querySelector('#nav-bars');
-
-        if (menuList.style.display === 'block') {
-            menuList.style.display = 'none';
-            navBars.classList.add('fa-bars');
-            navBars.classList.remove('fa-times');
-        } else {
-            menuList.style.display = 'block';
-            navBars.classList.remove('fa-bars');
-            navBars.classList.add('fa-times');
-        }
-  } */
-
+    document.querySelector('#menu-list').classList.toggle('oculto');
+ }
+ 
 }
