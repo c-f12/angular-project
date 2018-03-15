4b8fcd12bd9416ae5f4ff54593a4da2caa8d7240
clara fernandez <clara.fernandez@babel.es>
2018-03-15 13:57:57 +0100

    e2e tests

--- project-app/e2e/app.e2e-spec.ts
+++ project-app/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('project-app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Descubre la moda sostenible y ecológica');
   });
 });
